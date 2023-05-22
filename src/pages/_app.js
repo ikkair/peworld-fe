0447957fe3b25578992ce108d2b2c484c7994cbf
ikkair <ikhlasul.kamal.i@gmail.com>
2023-05-22 21:40:42 +0700

    fix build error

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,11 @@
-import { Html, Head, Script } from "next/document";
 import "../styles/global.css"
 // import "bootstrap/dist/css/bootstrap.css"
 // import "bootstrap/dist/js/bootstrap.js"
 
 export default function App({ Component, pageProps }) {
   return (
-    <Html>
+    <>
       <Component {...pageProps} />
-    </Html>
+    </>
   );
 }
